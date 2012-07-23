5569cc0fb96f8d5702689d9aaed972d31576c522
Mikalai Silivonik <mikalai@msilivonik.com>
2012-07-22 21:10:03 -0600

    removed unused code

--- SinglePageTest/Global.asax.cs
+++ SinglePageTest/Global.asax.cs
@@ -23,7 +23,6 @@ namespace SinglePageTest
 
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
